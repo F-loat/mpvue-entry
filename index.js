636755ee08f43329e95528d1ad9369307dcb168f
F-loat <945852046@qq.com>
2018-03-30 20:10:51 +0800

    修正引用的文件

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function transformCode (str) {
 }
 
 function genEntry (config_file) {
-  
+
   const pages = path.isAbsolute(config_file) ?
     require(config_file) : require(resolveApp(config_file))
 
@@ -26,15 +26,15 @@ function genEntry (config_file) {
 
   const template = transformCode(String(fs.readFileSync(entry.app)))
 
-  template.match(/export default ?({[^]*})/)
-
   pages.forEach((page) => {
     const entryFile = resolveModule(`./${page.name}.js`)
 
     const config = JSON.stringify(page.wx || {})
-    fs.writeFileSync(entryFile, template.replace(RegExp.$1, config))
+    fs.writeFileSync(entryFile, template
+      .replace(/import App from .*/, `import App from '@${page.path}'`)
+      .replace(/export default ?{[^]*}/, `export default ${config}`))
 
-    entry[page.path] = entryFile
+    entry[page.path.replace(/^\//, '')] = entryFile
   })
 
   return entry
