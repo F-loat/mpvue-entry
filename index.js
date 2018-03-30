1e98c28a21d0d8bab003f54c259c975c8adad2c6
F-loat <945852046@qq.com>
2018-03-30 17:50:10 +0800

    基本功能完成

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const path = require('path')
+const babel = require('babel-core')
+
+function resolveApp (dir) {
+  return path.join(path.dirname(require.main.filename), '..', dir)
+}
+
+function resolveModule (dir) {
+  return path.join(__dirname, dir)
+}
+
+function transformCode (str) {
+  const babelrc = resolveApp('./.babelrc')
+  return babel.transform(str, { extends: babelrc }).code
+}
+
+function genEntry (config_file) {
+  const pages = require(config_file)
+
+  const entry = {
+    app: resolveApp('./src/main.js')
+  }
+
+  const template = transformCode(String(fs.readFileSync(entry.app)))
+
+  template.match(/export default ?{([^]*)}/)
+
+  pages.forEach((page) => {
+    const entryFile = resolveModule(`./${page.name}.js`)
+
+    fs.writeFileSync(entryFile, template.replace(RegExp.$1, `${page.wx || ''}`))
+
+    entry[page.path] = entryFile
+  })
+
+  return entry
+}
+
+module.exports = genEntry
