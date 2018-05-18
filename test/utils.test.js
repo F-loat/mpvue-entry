9059ad605887ddd7a0924eaa41b19d5dd5882529
F-loat <945852046@qq.com>
2018-05-18 20:34:36 +0800

    test: 添加测试用例

---
+++ test/utils.test.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+const assert = require('assert');
+const utils = require('../src/utils');
+
+function resolveTest(dir) {
+  return path.join(__dirname, '../test', dir);
+}
+
+describe('utils', () => {
+  describe('resolveApp', () => {
+    it('should return a path relative to app', () => {
+      assert.equal(utils.resolveApp('./test'), resolveTest('../node_modules/mocha/test'));
+    });
+  });
+
+  describe('resolveModule', () => {
+    it('should return a path relative to module', () => {
+      assert.equal(utils.resolveModule('./test'), resolveTest('.'));
+    });
+  });
+
+  describe('isConfigChanged', () => {
+    const oldPages = [{
+      path: '/test/a',
+    }, {
+      path: '/test/b',
+      }];
+    const pageA = {
+      path: '/test/a',
+    };
+    const pageB = {
+      path: '/test/b',
+      config: {
+        enablePullDownRefresh: true,
+      },
+    };
+    it('should return false when the config is present', () => {
+      assert.equal(utils.isConfigChanged(pageA, oldPages), false);
+    });
+    it('should return true when the config is not present', () => {
+      assert.equal(utils.isConfigChanged(pageB, oldPages), true);
+    });
+  });
+
+  describe('genEntry', () => {
+    const paths = {
+      pages: resolveTest('./assets/pages.js'),
+      template: resolveTest('./assets/main.js'),
+      dist: resolveTest('../dist'),
+      entry: resolveTest('./temp'),
+      bakPages: resolveTest('./temp/pages.bak.js'),
+      bakTemplate: resolveTest('./temp/template.bak.js'),
+    };
+    const options = {
+      cache: true,
+    };
+    it('should return entry object', () => {
+      fs.mkdirSync(resolveTest('./temp'));
+      const entry = utils.genEntry(paths, options);
+      assert.equal(entry['app'], resolveTest('./assets/main.js'));
+      assert.equal(entry['pages/a'], resolveTest('./temp/pageA.js'));
+      rimraf(resolveTest('./temp'), (err) => {
+        if (err) console.log(err)
+      });
+    });
+  });
+});
